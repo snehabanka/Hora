de54778c7eefba2487e0b11f0f35d3f8f9bd506e
Harsha Meerchandani <Harsha.Meerchandani@visionetsystems.com>
2023-08-25 10:55:13 +0530

    drapwer changes

--- App.tsx
+++ App.tsx
@@ -2,13 +2,21 @@ import React from 'react';
 import {Text} from 'react-native'
 import { NavigationContainer } from '@react-navigation/native';
 import StackNavigation  from './navigation/StackNavigation'
-import DrawerNavigation from './navigation/DrawerNavigation';
+import DrawerNavigation from './components/Drawer';
+
 
 const App = () => {
   return ( 
     <NavigationContainer>
             <StackNavigation/>
     </NavigationContainer>
+  //    <GestureHandlerRootView style={{ flex: 1 }}>
+  //    <NavigationContainer initialRouteName="Contact">
+  //      {shouldUseDrawer ?
+  //        <DrawerNavigation />
+  //        : <StackNavigation />}
+  //    </NavigationContainer>
+  //  </GestureHandlerRootView>
   );
 };
 
